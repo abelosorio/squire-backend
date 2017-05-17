dbaf7f1cd0c1940c77a52d0ac7d2aedb5d5e39b4
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-05-17 18:57:13 -0300

    Add query to find all WorkEntries.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,12 +1,20 @@
 Types::QueryType = GraphQL::ObjectType.define do
   name 'Query'
 
-  field :workEntry do
+  field :work_entry do
     description 'Find a WorkEntry by ID'
     type Types::WorkEntryType
     argument :id, !types.ID
     resolve ->(obj, args, ctx) {
       WorkEntry.find args['id']
+    }
+  end
+
+  field :work_entries do
+    description 'Find all WorkEntries'
+    type Types::WorkEntryType.to_list_type
+    resolve -> (obj, args, ctx) {
+      WorkEntry.all
     }
   end
 end
