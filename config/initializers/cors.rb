8951563a04f58dd3360f9c767b3ccab6a7e4b568
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-05-23 20:23:54 -0300

    Change default CORS_ORIGIN.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS_ORIGINS'] || 'http://localhost:4000'
+    origins ENV['CORS_ORIGINS'] || 'http://localhost:3000'
 
     resource '*',
       headers: :any,
