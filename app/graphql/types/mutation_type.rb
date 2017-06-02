099452f4c4cb6c88821bc527994abfc2a42c19b2
Abel M. Osorio <abel.m.osorio@gmail.com>
2017-06-02 13:04:43 -0300

    Add UpdateWorkEntry mutation.

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -9,6 +9,15 @@ Types::MutationType = GraphQL::ObjectType.define do
     }
   end
 
+  field :update_work_entry, Types::WorkEntryType do
+    name 'UpdateWorkEntry'
+    argument :id, !types.Int
+    argument :work_entry, !WorkEntryInputType
+    resolve ->(object, input, ctx) {
+      WorkEntry.update input[:id], input[:work_entry].to_h
+    }
+  end
+
   field :delete_work_entry, types.Boolean do
     name 'DeleteWorkEntry'
     argument :id, !types.Int
